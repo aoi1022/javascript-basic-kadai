13146da33acb8c9f3c6004f482f36e92f9c0d855
aoi1022 <161836578+aoi1022@users.noreply.github.com>
2024-04-25 21:32:36 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+const date = new Date();
+// const [month, day, year] = [
+//   date.getMonth(),
+//   date.getDate(),
+//   date.getFullYear(),
+// ];
+const year = date.getFullYear();
+const month = date.getMonth()+1;
+const day = date.getDate();    
+console.log(year + "年" + month + "月" + day + "日");
